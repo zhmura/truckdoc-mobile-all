944b4d957ccad4b62a2c4c7a3f42e2fbf941c762
Alexei Osipov <Osipov.AV@gmail.com>
2020-05-06 12:18:40 +0300

    Update test

--- mobile-modules/truckdoccommon/truckdoc-client-api/src/test/java/com/sanda/truckdoc/client/api/v3/sync/instructions/model/InstructionSetWithVersionTest.java
+++ mobile-modules/truckdoccommon/truckdoc-client-api/src/test/java/com/sanda/truckdoc/client/api/v3/sync/instructions/model/InstructionSetWithVersionTest.java
@@ -20,6 +20,11 @@ public class InstructionSetWithVersionTest {
 
         InstructionSetWithVersion val = objectMapper.readValue(is, InstructionSetWithVersion.class);
         Assert.assertNotNull(val);
+        InstructionSetBranch node1 = (InstructionSetBranch) val.getInstructionSet().getEntries().get(1);
+        InstructionSetLeaf node2 = (InstructionSetLeaf) node1.getEntries().get(0);
+        Assert.assertNotNull(node2.getFile());
+        Assert.assertNotNull(node2.getFile().getFileId());
+
 
         is.close();
     }
