c1b72780b1b07bb7d383f26b21e6fb05edda2d9e
Siarhei Zhmura <siarhei+backbase@backbase.com>
2025-11-18 19:07:10 +0100

    github release flow updated

--- app-updater/src/main/java/com/sanda/truckdoc/updater/config/GitHubConfig.kt
+++ app-updater/src/main/java/com/sanda/truckdoc/updater/config/GitHubConfig.kt
@@ -17,10 +17,12 @@ object GitHubConfig {
         // Main TruckDoc client app
         const val CLIENT_PACKAGE_NAME = "com.sanda.truckdoc.client.default"
         const val CLIENT_APK_PATTERN = "truckdoc-client-v"
+        const val CLIENT_APK_SUFFIX = ".apk"  // No flavor suffix in release builds
         
         // Updater app itself
         const val UPDATER_PACKAGE_NAME = "com.sanda.truckdoc.updater"
         const val UPDATER_APK_PATTERN = "truckdoc-updater-v"
+        const val UPDATER_APK_SUFFIX = ".apk"
     }
 }
 
